506e322d45a14f855cf6a3090162caf13dab8b0d
ArvinZJC <tomzjc@qq.com>
2022-02-14 17:57:32 +0800

    feat: add comments regarding algorithm complexity

--- Python/1-Two_Sum/two_sum_1.py
+++ Python/1-Two_Sum/two_sum_1.py
@@ -1,11 +1,11 @@
 """
 '''
 Description: the solution to Problem 1 - brute force
-Version: 1.0.0.20220210
+Version: 1.0.1.20220213
 Author: Arvin Zhao
 Date: 2022-02-10 21:25:31
 Last Editors: Arvin Zhao
-LastEditTime: 2022-02-10 21:39:06
+LastEditTime: 2022-02-13 11:49:15
 '''
 """
 
@@ -14,6 +14,8 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """Time complexity: O(n_nums ^ 2), space complexity: O(1)."""
+
         n_nums = len(nums)
 
         for i in range(n_nums):
--- Python/1-Two_Sum/two_sum_2.py
+++ Python/1-Two_Sum/two_sum_2.py
@@ -1,17 +1,22 @@
 """
+'''
 Description: the solution to Problem 1 - hash table
-Version: 1.0.0.20220210
+Version: 1.0.1.20220213
 Author: Arvin Zhao
 Date: 2022-02-10 21:25:45
 Last Editors: Arvin Zhao
-LastEditTime: 2022-02-10 21:25:45
+LastEditTime: 2022-02-13 11:49:48
+'''
 """
+
 
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """Time complexity: O(n_nums), space complexity: O(n_nums)."""
+
         hash_table = {}
 
         for i, num in enumerate(nums):
