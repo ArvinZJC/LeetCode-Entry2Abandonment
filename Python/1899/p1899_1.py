793fd4ae78865fc5077532dfd55b5863e7a80fc6
ArvinZJC <tomzjc@qq.com>
2022-03-10 19:59:24 +0800

    feat: add the solution to Problem 1899

---
+++ Python/1899/p1899_1.py
@@ -0,0 +1,23 @@
+"""
+Description: Problem 1899 (Merge Triplets to Form Target Triplet) - Solution 1
+Version: 1.0.0.20220310
+Author: Arvin Zhao
+Date: 2022-03-10 19:33:29
+Last Editors: Arvin Zhao
+LastEditTime: 2022-03-10 19:33:30
+"""
+
+from typing import List
+
+
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        """Greedy: time complexity: O(n_triplets), space complexity: O(1)."""
+
+        a = b = c = 0
+
+        for a_i, b_i, c_i in triplets:
+            if a_i <= target[0] and b_i <= target[1] and c_i <= target[2]:
+                a, b, c = max(a, a_i), max(b, b_i), max(c, c_i)
+
+        return [a, b, c] == target
